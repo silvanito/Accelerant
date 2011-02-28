3d098f558719d4f39bb4241de41319131ce07dec
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-02-27 20:46:35 -0600

    no overwrite deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,16 +36,14 @@ role :db,  location, :primary => true
 
 
 namespace :deploy do
-  desc "Restarting mod_rails with restart.txt"
-  task :restart, :roles => :app, :except => { :no_release => true } do
+  desc "Restart Application"
+  task :restart, :roles => :app do
     run "touch #{current_path}/tmp/restart.txt"
   end
- 
-  [:start, :stop].each do |t|
-    desc "#{t} task is a no-op with mod_rails"
-    task t, :roles => :app do ; end
+  desc "Start Application -- not needed for Passenger"
+  task :start, :roles => :app do
+    #nothing -- need to override default cap start task when using passenger
   end
-
 end
 
 after "deploy:symlink", "deploy:update_crontab"
