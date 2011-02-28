47fc450299130470d525f2123f131708e92ad297
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-02-28 01:03:38 -0600

    change repository dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :deploy_to, "/home/chazzer/public_html/#{application}"
  
 default_run_options[:pty] = true
 #set :repository,  "git://github.com/chazzerguy/Accelerant.git"
-set :repository,  "git@github.com:chazzerguy/Accelerant.git"
+set :repository,  "git@github.com:silvanito/Accelerant.git"
  
  
 # If you aren't deploying to /u/apps/#{application} on the target
