bb99de89f8002d2ee26561e13756bf1240bb800b
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-03-03 00:40:37 -0600

    change migration for image_result and validation

--- db/migrate/20110303063200_change_column_imageresult_of_heatmaps.rb
+++ db/migrate/20110303063200_change_column_imageresult_of_heatmaps.rb
@@ -1,6 +1,6 @@
 class ChangeColumnImageresultOfHeatmaps < ActiveRecord::Migration
   def self.up
-    change_column(:heatmaps, :image_result, :binary, :size => 2.megabyte)
+    change_column(:heatmaps, :image_result, :binary, :limit => 10.megabyte)
   end
 
   def self.down
