ba6841751bc84c35a1bd4bd394b8230ff7396e06
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-04-28 02:58:46 -0500

    changing delay function

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 function delay(){
-  new Effect.Opacity('container', { from: 1.0, to: 0.4, duration: 0.4  });
+  $('container').setOpacity(0.4);
   $('notice').show();
 }
 
