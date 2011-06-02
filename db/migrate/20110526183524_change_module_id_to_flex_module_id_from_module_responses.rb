57192adc544772bd271e82d1baf4772e6b0eb764
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-06-02 18:01:32 -0500

    delete bad migrations

--- db/migrate/20110526183524_change_module_id_to_flex_module_id_from_module_responses.rb
+++
@@ -1,9 +0,0 @@
-class ChangeModuleIdToFlexModuleIdFromModuleResponses < ActiveRecord::Migration
-  def self.up
-    rename_column :module_responses, :module_id, :flex_module_id
-  end
-
-  def self.down
-    rename_column :module_responses, :flex_module_id, :module_id
-  end
-end
