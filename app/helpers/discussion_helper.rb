2e6336ed3b783d0d42749227e1208c3ae790b452
Bernardo Galindo <bernardo.galindo@crowdint.com>
2011-06-21 17:03:53 -0500

    add rule for admin/participant to show flex modules

--- app/helpers/discussion_helper.rb
+++ app/helpers/discussion_helper.rb
@@ -1,2 +1,17 @@
 module DiscussionHelper
+  def modules_show(discussion)
+    if self.current_user.admin || self.current_user.moderator
+      if discussion.flex_modules.empty?
+        return false
+      else
+        return true
+      end
+    else
+      if discussion.flex_modules.published.empty?
+        return false
+      else
+        return true
+      end
+    end
+  end
 end
