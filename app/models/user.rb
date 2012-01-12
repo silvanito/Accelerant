a5c8b9ee215c14e05f23274b48d6d43cbfd0dabc
David Madrigal <mc.david.madrigal@gmail.com>
2012-01-12 17:46:24 -0600

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,10 +18,26 @@ class User < ActiveRecord::Base
   named_scope :is_admin, :conditions => {:admin => true}
   named_scope :is_participant, :conditions => {:participant => true}
 
+  #has_attached_file :avatar,
+  #:whiny => false, 
+  #:whiny_thumbnails => false,
+  #:styles => { :large => "300x300>",:medium => "100x100>", :thumb => "80x80>", :small => "50x50>", :smaller => "30x30>", :tiny => "20x20>" }
+  
+  
   has_attached_file :avatar,
-  :whiny => false, 
-  :whiny_thumbnails => false,
-  :styles => { :large => "300x300>",:medium => "100x100>", :thumb => "80x80>", :small => "50x50>", :smaller => "30x30>", :tiny => "20x20>" }
+      :storage => :s3,
+      :bucket => 'blognog',
+      :s3_credentials => {
+        :access_key_id =>'AKIAJYKCWTZMXFO2YBNA',
+        :secret_access_key => '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'
+      },
+      :whiny => false, 
+      :whiny_thumbnails => false,
+      :styles => { :large => "300x300>",:medium => "100x100>", :thumb => "80x80>", :small => "50x50>", :smaller => "30x30>", :tiny => "20x20>" }
+      
+      
+      
+      
 
   validates_presence_of     :login
   validates_length_of       :login,    :within => 3..40
