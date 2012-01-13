6ca490888d30ca6b658efd51e7c1093e0beb8080
David Madrigal <mc.david.madrigal@gmail.com>
2012-01-13 01:09:17 -0600

    remove

--- vendor/plugins/picky-color/init.rb
+++
@@ -1,4 +0,0 @@
-require "picky_color"
-ActionView::Helpers::FormHelper.send :include, PickyColor::FormHelper
-ActionView::Base.send :include, PickyColor::FormHelper
-ActionView::Base.send :include, PickyColor::FormHelper
